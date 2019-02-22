fdb8dce6aa9aa7bc2124bf3c92071af4fdd76ce8
Nicholas Simon <36269798+Andinoriel@users.noreply.github.com>
2019-02-22 16:27:46 +0200

    Hotfix - deleted needless scripts

--- scripts/additional/preloader.js
+++
@@ -1,4 +0,0 @@
-$(window).on('load', function () {
-    var $preloader = $('.preloader');
-    $preloader.delay(2000).fadeOut('slow');
-});
--- scripts/additional/slick_news_content_control.js
+++
@@ -1,9 +0,0 @@
-var backgrounds_cards = [
-    "images/1.jpg",
-    "images/1.jpg",
-    "images/1.jpg"
-];
-var cards = $('.card');
-for (var i = 0; i < cards.length; i++) {
-    $(cards[i]).css({ "background": "url(" + backgrounds_cards[i] + ")", "background-size": "cover" });
-}
